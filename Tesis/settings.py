d9f3e83242b6d9cdaadc4a46f3672c7de0d71ced
CamiloRB <camilo.rebellon.g@gmail.com>
2021-02-28 04:00:46 -0500

    Cambios setting

--- Tesis/settings.py
+++ Tesis/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 
 #DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
 
-ALLOWED_HOSTS = ['192.168.0.18', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
